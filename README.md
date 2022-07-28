37615fed99b8f5b021d915f54432647cb6998f28
NicholasMurray <murray.nicholas@gmail.com>
2022-07-28 11:11:27 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-wwzybt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-wwzybt)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Emoji Snap! 🃏🃏🤪</h1>
+      <p>Lets play</p>
+    </div>
+  );
+}
